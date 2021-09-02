57052d0e72aac087733ed7f616e1f753ffae09ff
IfanTsai <1558499691@qq.com>
2021-09-02 23:15:17 +0800

    load config from file and environment variables

--- cmd/simple-bank-api/run.go
+++ cmd/simple-bank-api/run.go
@@ -4,20 +4,21 @@ import (
 	"database/sql"
 	"log"
 
+	"github.com/ifantsai/simple-bank-api/util"
+
 	"github.com/ifantsai/simple-bank-api/api"
 	db "github.com/ifantsai/simple-bank-api/db/sqlc"
 
 	_ "github.com/lib/pq"
 )
 
-const (
-	dbDriver      = "postgres"
-	dbSource      = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
-	serverAddress = "0.0.0.0:8080"
-)
-
 func main() {
-	conn, err := sql.Open(dbDriver, dbSource)
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load configurations:", err)
+	}
+
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err)
 	}
@@ -25,7 +26,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	if err := server.Start(serverAddress); err != nil {
+	if err := server.Start(config.ServerAddress); err != nil {
 		log.Fatal("cannot start server: ", err)
 	}
 }
