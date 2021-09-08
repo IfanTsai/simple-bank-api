661c503331f2d544b6df05f9dad33b779a6dffab
IfanTsai <1558499691@qq.com>
2021-09-09 06:37:28 +0800

    implement create user API

---
+++ api/user.go
@@ -0,0 +1,71 @@
+package api
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/ifantsai/simple-bank-api/util"
+
+	"github.com/gin-gonic/gin"
+	db "github.com/ifantsai/simple-bank-api/db/sqlc"
+	"github.com/lib/pq"
+)
+
+type createUserRequest struct {
+	Username string `json:"username" binding:"required,alphanum"`
+	Password string `json:"password" binding:"required,min=6"`
+	FullName string `json:"full_name" binding:"required"`
+	Email    string `json:"email" binding:"required,email"`
+}
+
+type createUserResponse struct {
+	Username          string    `json:"username"`
+	FullName          string    `json:"full_name"`
+	Email             string    `json:"email"`
+	PasswordChangedAt time.Time `json:"password_changed_at"`
+	CreatedAt         time.Time `json:"created_at"`
+}
+
+func (s *Server) createUser(c *gin.Context) {
+	var req createUserRequest
+	if err := c.ShouldBindJSON(&req); err != nil {
+		c.JSON(http.StatusBadRequest, errorResponse(err))
+
+		return
+	}
+
+	hashedPassword, err := util.HashPassword(req.Password)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, errorResponse(err))
+
+		return
+	}
+
+	user, err := s.store.CreateUser(c, db.CreateUserParams{
+		Username:       req.Username,
+		HashedPassword: hashedPassword,
+		FullName:       req.FullName,
+		Email:          req.Email,
+	})
+	if err != nil {
+		if pqErr, ok := err.(*pq.Error); ok {
+			switch pqErr.Code.Name() {
+			case "unique_violation":
+				c.JSON(http.StatusForbidden, errorResponse(err))
+
+				return
+			}
+		}
+		c.JSON(http.StatusInternalServerError, errorResponse(err))
+
+		return
+	}
+
+	c.JSON(http.StatusOK, createUserResponse{
+		Username:          user.Username,
+		FullName:          user.FullName,
+		Email:             user.Email,
+		PasswordChangedAt: user.PasswordChangedAt,
+		CreatedAt:         user.CreatedAt,
+	})
+}
---
+++ util/password.go
@@ -0,0 +1,21 @@
+package util
+
+import (
+	"github.com/pkg/errors"
+	"golang.org/x/crypto/bcrypt"
+)
+
+// HashPassword returns the bcrypt hash of the password
+func HashPassword(password string) (string, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), 10)
+	if err != nil {
+		return "", errors.Wrap(err, "failed to hash password")
+	}
+
+	return string(hashedPassword), nil
+}
+
+// CheckPassword checks if the provided password is correct or not
+func CheckPassword(password, hashedPassword string) error {
+	return bcrypt.CompareHashAndPassword([]byte(hashedPassword), []byte(password))
+}
