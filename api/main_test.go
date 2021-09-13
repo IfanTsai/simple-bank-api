a8b51520cf8def419625b43d4f9f10d4f105a701
IfanTsai <1558499691@qq.com>
2021-09-14 00:15:48 +0800

    implement login user API

--- api/main_test.go
+++ api/main_test.go
@@ -3,6 +3,11 @@ package api
 import (
 	"os"
 	"testing"
+	"time"
+
+	db "github.com/ifantsai/simple-bank-api/db/sqlc"
+	"github.com/ifantsai/simple-bank-api/util"
+	"github.com/stretchr/testify/require"
 
 	"github.com/gin-gonic/gin"
 )
@@ -10,4 +15,16 @@ import (
 func TestMain(m *testing.M) {
 	gin.SetMode(gin.TestMode)
 	os.Exit(m.Run())
+}
+
+func newTestServer(t *testing.T, store db.Store) *Server {
+	config := util.Config{
+		TokenSymmetricKey:   util.RandomString(32),
+		AccessTokenDuration: time.Minute,
+	}
+
+	server, err := NewServer(config, store)
+	require.NoError(t, err)
+
+	return server
 }
