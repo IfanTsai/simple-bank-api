063884f2ecde281b465fdb7fe35c711bb914472d
IfanTsai <i@caiyifan.cn>
2022-07-28 01:11:10 +0800

    validate grpc parameters

---
+++ gapi/error.go
@@ -0,0 +1,29 @@
+// nolint
+package gapi
+
+import (
+    "google.golang.org/genproto/googleapis/rpc/errdetails"
+    "google.golang.org/grpc/codes"
+    "google.golang.org/grpc/status"
+)
+
+type BadRequestFieldViolation = errdetails.BadRequest_FieldViolation
+
+func fieldViolation(field string, err error) *BadRequestFieldViolation {
+    return &errdetails.BadRequest_FieldViolation{
+        Field:       field,
+        Description: err.Error(),
+    }
+}
+
+func invalidParameters(violations []*BadRequestFieldViolation) error {
+    badRequest := &errdetails.BadRequest{FieldViolations: violations}
+    statusInvalid := status.New(codes.InvalidArgument, "invalid parameters")
+
+    statusDetails, err := statusInvalid.WithDetails(badRequest)
+    if err != nil {
+        return statusInvalid.Err()
+    }
+
+    return statusDetails.Err()
+}
