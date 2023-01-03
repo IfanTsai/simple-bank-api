5b8ad952c39c2b6905abcf5873c45fd21dbc721a
IfanTsai <i@caiyifan.cn>
2023-01-03 23:36:20 +0800

    move user creation and asynq sending into TX

--- gapi/rpc_create_user.go
+++ gapi/rpc_create_user.go
@@ -26,12 +26,25 @@ func (s *GRPCServer) CreateUser(ctx context.Context, req *pb.CreateUserRequest) 
 		return nil, status.Errorf(codes.Internal, "failed to hash password: %s", err)
 	}
 
-	user, err := s.store.CreateUser(ctx, db.CreateUserParams{
-		Username:       req.GetUsername(),
-		HashedPassword: hashedPassword,
-		FullName:       req.GetFullName(),
-		Email:          req.GetEmail(),
+	txResult, err := s.store.CreateUserTx(ctx, db.CreateUserTxParams{
+		CreateUserParams: db.CreateUserParams{
+			Username:       req.GetUsername(),
+			HashedPassword: hashedPassword,
+			FullName:       req.GetFullName(),
+			Email:          req.GetEmail(),
+		},
+		AfterCreate: func(user db.User) error {
+			taskPayload := &worker.PayloadSendVerifyEmail{Username: req.GetUsername()}
+			taskAsynqOpts := []asynq.Option{
+				asynq.MaxRetry(10),
+				asynq.ProcessIn(1 * time.Second),
+				asynq.Queue(worker.QueueCritical),
+			}
+
+			return s.taskDistributor.DistributeTaskSendVerifyEmail(ctx, taskPayload, taskAsynqOpts...)
+		},
 	})
+
 	if err != nil {
 		if pqErr, ok := err.(*pq.Error); ok { //nolint: errorlint
 			switch pqErr.Code.Name() {
@@ -43,18 +56,8 @@ func (s *GRPCServer) CreateUser(ctx context.Context, req *pb.CreateUserRequest) 
 		return nil, status.Errorf(codes.Internal, "failed to create user: %s", err)
 	}
 
-	taskPayload := &worker.PayloadSendVerifyEmail{Username: req.GetUsername()}
-	taskAsynqOpts := []asynq.Option{
-		asynq.MaxRetry(10),
-		asynq.ProcessIn(1 * time.Second),
-		asynq.Queue(worker.QueueCritical),
-	}
-	if err := s.taskDistributor.DistributeTaskSendVerifyEmail(ctx, taskPayload, taskAsynqOpts...); err != nil {
-		return nil, status.Errorf(codes.Internal, "failed to distribute task to send verify email: %s", err)
-	}
-
 	return &pb.CreateUserResponse{
-		User: convertUser(&user),
+		User: convertUser(&txResult.User),
 	}, nil
 }
 
